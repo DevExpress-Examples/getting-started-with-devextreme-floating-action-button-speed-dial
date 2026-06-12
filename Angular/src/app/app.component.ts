04099fba9833c29ea694dd4ea28685b3c7d5f673
AlisherAmonulloev <AlisherAmonulloev@users.noreply.github.com>
2026-06-12 14:20:26 +0300

    Update Angular to v22

--- Angular/src/app/app.component.ts
+++ Angular/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ChangeDetectionStrategy } from '@angular/core';
 import notify from 'devextreme/ui/notify';
 import config from 'devextreme/core/config';
 import { FloatingActionButtonConfig, NotificationOptions } from './app.types';
@@ -9,6 +9,7 @@ import { DxSpeedDialActionModule } from 'devextreme-angular/ui/speed-dial-action
     selector: 'app-root',
     imports: [DxSpeedDialActionModule],
     templateUrl: './app.component.html',
+    changeDetection: ChangeDetectionStrategy.Eager,
     styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
